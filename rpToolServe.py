17985d7f7e4f67a80beb8db4f6f82f54ec9bae0c
mdulac <melchior.dulac@inra.fr>
2020-06-23 22:38:34 +0200

    update

--- rpToolServe.py
+++ rpToolServe.py
@@ -6,6 +6,13 @@ import tarfile
 import glob
 import logging
 
+logging.basicConfig(
+    #level=logging.DEBUG,
+    level=logging.WARNING,
+    format='%(asctime)s.%(msecs)03d %(levelname)s %(module)s - %(funcName)s: %(message)s',
+    datefmt='%d-%m-%Y %H:%M:%S',
+)
+
 sys.path.insert(0, '/home/')
 import rpSBML
 import rpTool
