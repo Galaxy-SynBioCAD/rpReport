755a0232199311d4ce7132a8b6277f440b537114
mdulac <melchior.dulac@inra.fr>
2020-07-21 13:59:50 +0200

    added the normalised pathway FBA

--- rpToolServe.py
+++ rpToolServe.py
@@ -7,14 +7,13 @@ import glob
 import logging
 
 logging.basicConfig(
-    #level=logging.DEBUG,
+    level=logging.DEBUG,
     #level=logging.WARNING,
-    level=logging.ERROR,
+    #level=logging.ERROR,
     format='%(asctime)s.%(msecs)03d %(levelname)s %(module)s - %(funcName)s: %(message)s',
     datefmt='%d-%m-%Y %H:%M:%S',
 )
 
-sys.path.insert(0, '/home/')
 import rpSBML
 import rpTool
 
